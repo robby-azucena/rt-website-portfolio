6209bd4c23a7d0c9a0e87d5c7de8be1649cf119d
Robby <robby.azucena@gmail.com>
2020-10-24 15:07:57 +0800

    created navbar in desktop

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import './scss/index.scss'
 import 'bootstrap/dist/css/bootstrap-grid.css'
+import 'rellax';
 
 import './image/home-banner.jpg'
-import 'rellax';
+import './image/rt-logo.svg'
+
+
 
 let Rellax = require('rellax');
 
-var rellax = new Rellax('.rellax',{
-    speed: -2,
+var rellax = new Rellax('.backgroundIndex-banner',{
+    speed: 1,
     center: false,
     wrapper: null,
     round: true,
