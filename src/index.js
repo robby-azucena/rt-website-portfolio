ef824788706ba95f1ffaa642a8e83096a874d09f
Robby <robby.azucena@gmail.com>
2020-10-24 14:02:27 +0800

    Checkpoint

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 import './scss/index.scss'
 import 'bootstrap/dist/css/bootstrap-grid.css'
-import 'axios'
 
-const axios = require('axios');
+import './image/home-banner.jpg'
 
-
-
-
-axios.get('http://worldtimeapi.org/api/timezone/Asia/Manila')
-  .then(function (response) {
-    if (response){
-        console.log(response.data.timezone)
-        document.getElementById("timezone").innerHTML = response.data.timezone;
-        document.getElementById("time").innerHTML = response.data.utc_datetime;  
-    }
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
